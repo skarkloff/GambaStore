dad16159d6bdc8c774134a6779382d9dc2b25bf0
Lautaro Skarkloff <skarkloff.ls99@gmail.com>
2026-04-16 19:17:13 -0300

    Full boostrap en api

--- api/index.php
+++ api/index.php
@@ -1,2 +1,30 @@
 <?php
-require __DIR__ . '/../public/index.php';
+
+define('LARAVEL_START', microtime(true));
+
+// Register the Composer autoloader
+require __DIR__ . '/../vendor/autoload.php';
+
+// Bootstrap Laravel
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// Register all service providers manually
+$app->bootstrapWith([
+    \Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables::class,
+    \Illuminate\Foundation\Bootstrap\LoadConfiguration::class,
+    \Illuminate\Foundation\Bootstrap\HandleExceptions::class,
+    \Illuminate\Foundation\Bootstrap\RegisterFacades::class,
+    \Illuminate\Foundation\Bootstrap\RegisterProviders::class,
+    \Illuminate\Foundation\Bootstrap\BootProviders::class,
+]);
+
+// Handle the request
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
