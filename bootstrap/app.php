df66742e4b3a27350bf69f01b08d90e158b3b5b2
zappe02 <patriciozappellini02@gmail.com>
2026-06-18 20:56:42 -0300

    cambios middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -27,6 +27,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'admin.role'  => \App\Http\Middleware\AdminRole::class,
             'auth.google' => \App\Http\Middleware\VerifyGoogleToken::class,
         ]);
+        $middleware->statefulApi();
         $middleware->api(prepend: [
             \Illuminate\Http\Middleware\HandleCors::class,
         ]);
