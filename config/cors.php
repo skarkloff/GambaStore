b0dd1e8e2bc153a8803dcdc1ffbebaf62576b0d1
zappe02 <patriciozappellini02@gmail.com>
2026-06-18 21:24:23 -0300

    cambios middleware

--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,16 @@
 <?php
 
 return [
-    'paths'                    => ['api/*'],
+    'paths'                    => ['api/*', 'api/api/*'], 
+    
     'allowed_methods'          => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     'allowed_origins'          => explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,http://localhost:5173')),
     'allowed_origins_patterns' => [],
-    'allowed_headers'          => ['Content-Type', 'Authorization', 'Accept', 'X-Requested-With'],
+    
+    'allowed_headers'          => ['*'], 
+    
     'exposed_headers'          => [],
     'max_age'                  => 86400,
-    'supports_credentials'     => false,
+    
+    'supports_credentials'     => true, 
 ];
